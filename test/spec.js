9e4998bb86fa5fc183e1e7e3445b5c7c93352294
Zhuoyuan Zhang <zhuoyuan@compass.com>
2017-07-11 14:12:24 -0400

    Setup mocha unit test

--- test/spec.js
+++ test/spec.js
@@ -1,15 +1,16 @@
-import toStartCaseStr from 'public/uc/toStartCaseStr.js';
+import assert from 'assert';
+import toStartCaseStr from '../src/index.js';
 
 describe('uc.toStartCaseStr', () => {
   it('should convert strings to sentence case', () => {
-    expect(toStartCaseStr('Management')).toBe('Management');
-    expect(toStartCaseStr('management')).toBe('Management');
-    expect(toStartCaseStr('managementCompanies')).toBe('Management Companies');
-    expect(toStartCaseStr('management_companies')).toBe('Management Companies');
+    assert.strictEqual(toStartCaseStr('Management'), 'Management');
+    assert.strictEqual(toStartCaseStr('management'), 'Management');
+    assert.strictEqual(toStartCaseStr('managementCompanies'), 'Management Companies');
+    assert.strictEqual(toStartCaseStr('management_companies'), 'Management Companies');
   });
 
   it('should handle correctly with single quote and dash', () => {
-    expect(toStartCaseStr('hell\'s kitchen')).toBe('Hell\'s Kitchen');
-    expect(toStartCaseStr('nice-to-have')).toBe('Nice-to-have');
+    assert.strictEqual(toStartCaseStr('hell\'s kitchen'), 'Hell\'s Kitchen');
+    assert.strictEqual(toStartCaseStr('nice-to-have'), 'Nice-to-have');
   });
 });
